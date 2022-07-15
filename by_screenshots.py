e4ba79684c4fe58263431a20692d136f201e971f
das67333 <kazimov.sp@phystech.edu>
2022-07-15 16:05:57 +0300

    minor refactoring

--- by_screenshots.py
+++ by_screenshots.py
@@ -6,31 +6,22 @@ import pyautogui
 import PIL.Image
 import mss
 
+pos = (1440, 540)
+mouse_pointer_shift = 20
 
 clicks = 5
 with mss.mss() as sct:
     m = sct.monitors[1]
-    box = (m['left'] + 1460, m['top'] + 540, m['left'] + 1461, m['top'] + 541)
+    box = (m['left'] + pos[0] + mouse_pointer_shift, m['top'] + pos[1],
+           m['left'] + pos[0] + 1 + mouse_pointer_shift, m['top'] + pos[1] + 1)
 
     while clicks:
         sct_img = sct.grab(box)
-        r, g, b = PIL.Image.frombytes("RGB", sct_img.size, sct_img.rgb).load()[0, 0]
+        r, g, b = PIL.Image.frombytes(
+            "RGB", sct_img.size, sct_img.rgb).load()[0, 0]
         if g / (b + 1) > 1.2 and g / (r + 1) > 1.5:
-            pyautogui.click(x=1440, y=540)
+            pyautogui.click(x=pos[0], y=pos[1])
             time.sleep(0.5)
-            pyautogui.click(x=1440, y=540)
+            pyautogui.click(x=pos[0], y=pos[1])
             time.sleep(0.5)
             clicks -= 1
-
-
-# while clicks:
-
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#     b, g, r = np.mean(frame[240, :], axis=0)
-#     if g / (b + 1) > 1.2 and g / (r + 1) > 1.5:
-#         pyautogui.click(x=1440, y=540)
-#         time.sleep(0.5)
-#         pyautogui.click(x=1440, y=540)
-#         time.sleep(0.5)
-#         clicks -= 1
